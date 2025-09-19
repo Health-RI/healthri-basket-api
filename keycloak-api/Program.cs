0a534b4d1ca23acd92b7913db3cb350d52d30611
Sean Berrie <Sean.Berrie@health-ri.nl>
2025-09-19 14:02:04 +0200

    added keycloak-api project added keycloak docker-compose file

---
+++ keycloak-api/Program.cs
@@ -0,0 +1,29 @@
+﻿using healthri_basket_api.Interfaces;
+using healthri_basket_api.Repositories;
+using healthri_basket_api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register in-memory services
+builder.Services.AddSingleton<IBasketRepository, InMemoryBasketRepository>();
+builder.Services.AddSingleton<ITransactionLogger, InMemoryTransactionLogger>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+

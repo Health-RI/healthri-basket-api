761ad59cb4f7d88cd101b78a3881f5c69a73543e
Sean Berrie <Sean.Berrie@health-ri.nl>
2025-09-23 14:48:21 +0200

    Added postgresql integration

---
+++ healthri-basket-api/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using healthri_basket_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace healthri_basket_api.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options){}
+    }
+}
--- healthri-basket-api/Program.cs
+++ healthri-basket-api/Program.cs
@@ -1,9 +1,14 @@
+using healthri_basket_api.Database;
 using healthri_basket_api.Interfaces;
 using healthri_basket_api.Repositories;
 using healthri_basket_api.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +20,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IBasketRepository, InMemoryBasketRepository>();
 builder.Services.AddSingleton<ITransactionLogger, InMemoryTransactionLogger>();
 builder.Services.AddScoped<IBasketService, BasketService>();
+
+// PostgreSQL database setup (https://www.npgsql.org/efcore/index.html?tabs=onconfiguring)
+var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
+
+builder.Services.AddDbContextPool<AppDbContext>(opt =>
+    opt.UseNpgsql(connectionString));
 
 builder.Services.AddAuthentication(options =>
 {

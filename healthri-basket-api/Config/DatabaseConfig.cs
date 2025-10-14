d00de62727a342651c53e5dfb019f33b6a5d379f
Sean Berrie <Sean.Berrie@health-ri.nl>
2025-10-14 09:18:53 +0200

    improvement: moved environment variable loading logic from program.cs to Config file.

---
+++ healthri-basket-api/Config/DatabaseConfig.cs
@@ -0,0 +1,21 @@
+﻿using dotenv.net;
+
+namespace healthri_basket_api.Config;
+
+public class DatabaseConfig
+{
+    public string? ConnectionString { get; set; }
+
+    public static DatabaseConfig LoadFromEnv()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new Exception("Database connection string environment variable is missing.");
+        }
+        return new DatabaseConfig
+        {
+            ConnectionString = connectionString
+        };
+    }
+}
--- healthri-basket-api/Program.cs
+++ healthri-basket-api/Program.cs
@@ -1,3 +1,5 @@
+using dotenv.net;
+using healthri_basket_api.Config;
 using healthri_basket_api.Database;
 using healthri_basket_api.Interfaces;
 using healthri_basket_api.Repositories;
@@ -5,7 +7,6 @@ using healthri_basket_api.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using dotenv.net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,46 +21,29 @@ builder.Services.AddScoped<ITransactionLogger, TransactionLoggerRepository>();
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IBasketService, BasketService>();
 
-// Get environment variables
+// Initialize config from environment variables
 DotEnv.Load();
-string? dbConnectionString = Environment.GetEnvironmentVariable("POSTGRESQL_CONNECTION_URL");
-string? openIdAuthority = Environment.GetEnvironmentVariable("OPENID_AUTHORITY");
-string? openIdIssuer = Environment.GetEnvironmentVariable("OPENID_ISSUER");
-string? openIdAudience = Environment.GetEnvironmentVariable("OPENID_AUDIENCE");
-string? openIdClientId = Environment.GetEnvironmentVariable("OPENID_CLIENT_ID");
-string? openIdClientSecret = Environment.GetEnvironmentVariable("OPENID_CLIENT_SECRET");
-
-if (string.IsNullOrEmpty(dbConnectionString))
-    throw new Exception("POSTGRESQL_CONNECTION_URL environment variable is not set.");
-if (string.IsNullOrEmpty(openIdAuthority))
-    throw new Exception("OPENID_AUTHORITY environment variable is not set.");
-if (string.IsNullOrEmpty(openIdIssuer))
-    throw new Exception("OPENID_ISSUER environment variable is not set.");
-if (string.IsNullOrEmpty(openIdAudience))
-    throw new Exception("OPENID_AUDIENCE environment variable is not set.");
-if (string.IsNullOrEmpty(openIdClientId))
-    throw new Exception("OPENID_CLIENT_ID environment variable is not set.");
-if (string.IsNullOrEmpty(openIdClientSecret))
-    throw new Exception("OPENID_CLIENT_SECRET environment variable is not set.");
+var dbConfig = DatabaseConfig.LoadFromEnv();
+var openIdConfig = OpenIdConfiguration.LoadFromEnv();
 
 // PostgreSQL setup
 builder.Services.AddDbContextPool<AppDbContext>(opt =>
-    opt.UseNpgsql(dbConnectionString));
+    opt.UseNpgsql(dbConfig.ConnectionString));
 
 // JWT Authentication setup
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(jwtOptions =>
 {
     jwtOptions.RequireHttpsMetadata = false;
-    jwtOptions.Authority = openIdAuthority;
-    jwtOptions.Audience = openIdAudience;
+    jwtOptions.Authority = openIdConfig.Authority;
+    jwtOptions.Audience = openIdConfig.Audience;
     jwtOptions.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
         ValidateAudience = true,
         ValidateIssuerSigningKey = true,
-        ValidAudiences = [openIdAudience],
-        ValidIssuers = [openIdIssuer]
+        ValidAudiences = [openIdConfig.Audience],
+        ValidIssuers = [openIdConfig.Issuer]
     }; 
     jwtOptions.MapInboundClaims = false;
 });
